573dd4e50d5d138c371f195cb320bdf8836bd359
liuyuhui <84964308@qq.com>
2019-04-12 10:14:51 +0800

    引入element ui

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 
 import Vue from 'vue';
 import store from './store';
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import router from './router';
 import './axios';
 import mixin from './mixin';
 
 import App from './App.vue';
+import './plugins/element.js'
 /* eslint-disable */
 if (process.env.NODE_ENV === 'development') {
     require('./mock/mock.js');
